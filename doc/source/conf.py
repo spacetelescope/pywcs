080e5eeb714e603883ac97409d43dbf454c45387
mdroe <mdroe@stsci.edu>
2009-11-23 20:46:54 +0000

    Update Sphinx docs to use new templates.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pywcs@1173 90a0a646-be8a-0410-bb88-9290da87bc01
    
    Former-commit-id: a8a24cb03ca2f4e732bef1d682473dccc768faa3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,6 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+from stsci_sphinxext.conf import *
+
 import sys, os
 import pywcs
 
@@ -23,7 +25,7 @@ import pywcs
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
+# extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -93,7 +95,7 @@ autoclass_content = 'both'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+# html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
