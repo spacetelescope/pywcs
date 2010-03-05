6fbd9d593c542668df283aa8026b34a8002397dc
mdroe <mdroe@stsci.edu>
2010-03-05 16:26:36 +0000

    Fix Python < 2.5 compatibility.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/astrolib/trunk/pywcs@1228 90a0a646-be8a-0410-bb88-9290da87bc01
    
    Former-commit-id: ee0cbb686fbc67345ad3df718f5162d8ec6ae1ab

--- src/pyutil.h
+++ src/pyutil.h
@@ -49,10 +49,12 @@ DAMAGE.
 /* Py_ssize_t for old Pythons */
 /* This code is as recommended by: */
 /* http://www.python.org/dev/peps/pep-0353/#conversion-guidelines */
-#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
-typedef int Py_ssize_t;
-# define PY_SSIZE_T_MAX INT_MAX
-# define PY_SSIZE_T_MIN INT_MIN
+#if PY_VERSION_HEX < 0x02050000 
+   #if !defined(PY_SSIZE_T_MIN)
+      typedef int Py_ssize_t;
+      # define PY_SSIZE_T_MAX INT_MAX
+      # define PY_SSIZE_T_MIN INT_MIN
+   #endif
 #define lenfunc inquiry
 #define ssizeargfunc intargfunc
 #define ssizeobjargproc intobjargproc
